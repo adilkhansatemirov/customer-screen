7e9370215d4e5256c199915f4ebccac5a1559737
dias0n <diasonmc271@gmail.com>
2026-01-03 21:08:48 +0500

    full kz language + video for correct scan

--- View/CustomerScreenWindow.xaml.cs
+++ View/CustomerScreenWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace Resto.Front.Api.CustomerScreen.View
         public event PropertyChangedEventHandler PropertyChanged;
 
         private ScreenType currentScreen = ScreenType.Welcome;
+
         public ScreenType CurrentScreen
         {
             get => currentScreen;
